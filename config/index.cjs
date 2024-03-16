a4f3b5079a4b849928dd6c15e217524deff35674
yinshiM <73021665+yinshiM@users.noreply.github.com>
2024-03-17 00:44:46 +0800

    Update index.cjs
    feat:L7公众号测试

--- config/index.cjs
+++ config/index.cjs
@@ -8,35 +8,35 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx1df09e4ac8394015',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'b05b5c9c976b6a5d2ed841c21904f050',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '福建',
+  CITY: '建瓯',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '/:pig',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oxJcP6-bZG-Yhb9pxuzDM0QLYgYM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'IxWybNYMTUAGjCHikLVr5oZom-gpV6MpFizIYwH_-gI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-11',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '/:pig', year: '2001', date: '12-11',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: 'Lee', year: '2000', date: '11-22',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2024', date: '02-02',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'Lee',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oxJcP6-bZG-Yhb9pxuzDM0QLYgYM',
     }
   ],
 
